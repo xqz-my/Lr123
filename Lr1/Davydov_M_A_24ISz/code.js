8ce2b50c092857f9a6024efb143df347f91d163d
mkhl.dvdv <mkhl.dvdv@yahoo.com>
2025-02-20 16:35:17 +0300

    fixed the Lr1 algorithm

--- Lr1/Davydov_M_A_24ISz/code.js
+++ Lr1/Davydov_M_A_24ISz/code.js
@@ -7,17 +7,27 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question(`Type the string, please:\n`, theOldString => {
-  let prevSymbol = null;
+function removeSiblings(theOldString) {
+  let encountered = new Set();
   let theNewString = "";
 
   for (const i in theOldString) {
-    if (theOldString[i] != prevSymbol) {
+    if (!encountered.has(theOldString[i])) {
       theNewString += theOldString[i];
-      prevSymbol = theOldString[i];
+      encountered.add(theOldString[i]);
     }
   }
+  return theNewString;
+}
 
+const alphabet = "A a B b C c D d E e F f G g H h I i J j K k L l M m N n O o" 
+  + "P p Q q R r S s T t U u V v W w X x Y y Z z this line is not going to"
+  + " appear on the screen"
+
+console.log(`the alphabet is:\n${removeSiblings(alphabet)}`)
+
+rl.question(`Type the string, please:\n`, theOldString => {
+  const theNewString = removeSiblings(theOldString);
   console.log(`The new string is: ${theNewString}`)
   rl.close();
 });
